89b92ac1330aea9584b5a2c1f0c374dd459a3d22
grzegorz <grzegorz@e-gaming.cz>
2019-11-19 14:36:20 +0100

    propper maching

--- open_project.sh
+++ open_project.sh
@@ -4,7 +4,7 @@ PROJECT_KEY=$1
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-PROJECT=$(grep "$PROJECT_KEY" "$CURRENT_DIR"/projects_map.list | head -n 1)
+PROJECT=$(grep -E "^$PROJECT_KEY" "$CURRENT_DIR"/projects_map.list | head -n 1)
 if [ ! -z "$PROJECT" ]; then
    EDITOR=$(echo "$PROJECT" | awk '{print $3}')
    DIR=$(echo "$PROJECT" | awk '{print $5}')
