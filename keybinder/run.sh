671e5c12005ef41e9736221a88d755fb7b320507
Grzegorz Klimek <grzegorz@e-gaming.cz>
2023-03-03 15:50:50 +0100

    fix keybinder

--- keybinder/run.sh
+++ keybinder/run.sh
@@ -4,11 +4,33 @@
 
 export KEYBINDER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-PID=$(ps aux | grep keybinder.py | grep -v grep)
+PROCCESS=$(ps aux | grep keybinder.py | grep -v grep)
 
-if [[ ! -z "$PID" ]]; then
+if [[ ! -z "$PROCCESS" ]]; then
   echo "keybinder.py already working"
-  echo "$PID"
-else
-  python3 "$KEYBINDER_DIR"/keybinder.py
+  echo "$PROCCESS"
+
+  if [[ "$1" == "restart" ]]; then
+    PID=$(echo "$PROCCESS" | awk '{ print $2 } ')
+
+    echo "killing keybinder on PID: $PID"
+
+    if [[ -n "$PID" ]]; then
+      kill "$PID"
+    fi
+  else
+    echo "Run with:"
+    echo ' '
+    echo '```'
+    echo "$0 restart"
+    echo '```'
+    echo ' '
+    echo "to restart keybinder"
+  fi
+fi
+
+if [[ "$1" == "restart" ]]; then
+
+  python3 "$KEYBINDER_DIR"/keybinder.py &
+  echo "Keybinder started"
 fi
